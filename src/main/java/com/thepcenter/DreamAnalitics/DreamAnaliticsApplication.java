378028279f84a42b41741378e91574f44d9b8a53
ROZ19K99 <mariafermedinamojica@gmail.com>
2025-04-07 20:41:38 -0500

    Subo cambios con funcionalidad de clonar sueños

---
+++ src/main/java/com/thepcenter/DreamAnalitics/DreamAnaliticsApplication.java
@@ -0,0 +1,13 @@
+package com.thepcenter.DreamAnalitics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamAnaliticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DreamAnaliticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thepcenter/DreamAnalitics/DreamAnaliticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thepcenter.DreamAnalitics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamAnaliticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
